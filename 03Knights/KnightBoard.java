001364a60d43366bbd448a960e2f2d44dbd3d7d7
Karen Chen <kchen6600@gmail.com>
2017-02-15 12:28:50 -0500

    working on solveH

--- 03Knights/KnightBoard.java
+++ 03Knights/KnightBoard.java
@@ -1,8 +1,15 @@
 public class KnightBoard{
 
     private int[][]board;
+    private String str = "";
 
     public KnightBoard(int startingRows, int startingCols){
+	board = new int[startingRows][startingCols];
+	for (int i = 0; i < board.length; i++){
+	    for (int j = i; j < board[i].length; j++){
+		board[i][j] = 0;
+	    }
+	}
     }
 
     public String toString(){
@@ -13,5 +20,59 @@ public class KnightBoard{
     }
 
     private boolean solveH(int row, int col, int level){
+       	if (level > row*col){
+	    return true;
+	}
+	if (board[row][col] != 0){
+	    return false;
+	}
+
+	board[row][col] = level;
+
+        if (canBePlaced(row+1, col+2) && solveH(row+1, col+2, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row-1, col-2) && solveH(row-1, col-2, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row+2, col+1) && solveH(row+2, col+1, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row-2, col-1) && solveH(row-2, col-1, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row+1, col-2) && solveH(row+1, col-2, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row-1, col +2) && solveH(row-1, col+2, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row+2, col-1) && solveH(row+2, col-1, level+1)){
+	    return true;
+	}
+
+	if (canBePlaced(row-2, col+1) && solveH(row-2, col+1, level+1)){
+	    return true;
+	}
+
+	board[row][col] = 0;
+	return false;
+    }
+
+
+    private boolean canBePlaced(int row, int col){
+	if (board[row][col] == 0){
+	    return true;
+	}
+	return false;
+    }
+
+    public static void main(String[]args){
     }
 }
