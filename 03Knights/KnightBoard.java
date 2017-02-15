8a8847ba820a0a34954692ade744d3696fffe384
Karen Chen <kchen6600@gmail.com>
2017-02-15 12:01:45 -0500

    functions outlined

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,17 @@
+public class KnightBoard{
+
+    private int[][]board;
+
+    public KnightBoard(int startingRows, int startingCols){
+    }
+
+    public String toString(){
+    }
+
+    public void solve(){
+	solve(0,0,1);
+    }
+
+    private boolean solveH(int row, int col, int level){
+    }
+}
