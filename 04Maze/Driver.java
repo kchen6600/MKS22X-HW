eda0b1b7713c74548f15e0f0f5809c687c36b196
Karen Chen <kchen6600@gmail.com>
2017-03-05 12:03:48 -0500

    tests

--- 04Maze/Driver.java
+++ 04Maze/Driver.java
@@ -5,8 +5,28 @@ public class Driver{
         f = new Maze("data1.dat");//true animates the maze.
         
         f.setAnimate(true);
-        f.solve();
+	f.solve();
 
-        System.out.println(f);
+         System.out.println(f);
+
+	Maze l;
+	l = new Maze("Maze1.txt");
+	l.setAnimate(true);
+	l.solve();
+	System.out.println(l);
+
+	//	Maze f2;
+	// f2 = new Maze("data2.dat");//true animates the maze.
+        
+	// f2.setAnimate(true);
+	//	f2.solve();
+	//	System.out.println(f2);
+
+       	Maze f3;
+        f3 = new Maze("data3.dat");//true animates the maze.
+        
+        f3.setAnimate(true);
+	f3.solve();
+	System.out.println(f3);
     }
 }
