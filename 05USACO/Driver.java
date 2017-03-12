f211377ee6e1c4644e0a7eea8b2de6ad5358ff98
Karen Chen <kchen6600@gmail.com>
2017-03-12 10:30:42 -0400

    tests

--- 05USACO/Driver.java
+++ 05USACO/Driver.java
@@ -1,6 +1,7 @@
 public class Driver{
     public static void main(String[] args){
 	USACO x = new USACO();
-	System.out.println(x.bronze("testfile1.txt")); //34211
+	//	System.out.println(x.bronze("testfile1.txt")); //34211
+	System.out.println(x.silver("siltestfile1.txt"));
     }
 }
