a5d498cbf6e2dd598927eafa47ba68d0ced512de
Karen Chen <kchen6600@gmail.com>
2017-03-12 10:39:22 -0400

    tests

--- 05USACO/Driver.java
+++ 05USACO/Driver.java
@@ -2,6 +2,15 @@ public class Driver{
     public static void main(String[] args){
 	USACO x = new USACO();
 	//	System.out.println(x.bronze("testfile1.txt")); //34211
-	System.out.println(x.silver("siltestfile1.txt"));
+        System.out.println(x.silver("siltestfile1.txt")); //1
+	System.out.println(x.silver("siltestfile2.txt")); //74
+	System.out.println(x.silver("siltestfile3.txt")); //6435
+	System.out.println(x.silver("siltestfile4.txt")); //339246
+	System.out.println(x.silver("siltestfile5.txt")); //0
+       	System.out.println(x.silver("siltest6.txt")); //14396412
+	System.out.println(x.silver("siltest7.txt")); //1533810
+        System.out.println(x.silver("siltest8.txt")); //456055
+	System.out.println(x.silver("siltest9.txt")); //28
+	System.out.println(x.silver("siltest10.txt")); //1321670
     }
 }
