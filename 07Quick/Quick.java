9e878bcdf1c77243946a699ad12c5a5d1316bfe8
Karen Chen <kchen6600@gmail.com>
2017-03-15 22:45:35 -0400

    doesn't work for repeated numbers yet

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,35 +1,41 @@
-import java.util.Random;
 public class Quick{
+
+    public static void swap(int[]data, int first, int second){
+	int t = data[first];
+	data[first] = data[second];
+	data[second] = t;
+    }
+    
     public static int part(int[] data, int start, int end){
-	int pivot = new Random().nextInt(end-start);
+	int pivot = (int)(Math.random() * data.length);
+
+	swap(data, pivot, end-1);
+
 	
-
-	int begin = start - 1;
-
-	for(int i = start; i <= end - 1; i++){
-	    if(data[i] <= data[pivot]){ //if element is less than or equal to element at the pivot
-		begin+=1;
+	int begin = start;
+	for(int i = start; i < end - 1; i++){
+	    if(data[i] < data[end-1]){ //if element is less than or equal to element at the pivot
 		//swap
-		int temp = data[begin];
-		data[begin] = data[i];
-		data[i] = temp;
+		swap(data, begin, i);
+		begin+=1;
+
+	      
 	    }
 	}
 	//swap
-	int temp = data[begin+1];
-	data[begin+1] = data[end];
-	data[end] = temp;
+	swap(data, begin, end-1);
 
-	return pivot;
+	return begin;
 		
     }
 
 
     public static int quickselect(int[]data, int k){
+	return -1;
     }
     public static void main(String[]args){
 
-	int[] arr = new int[]{1, 3, 10, 11, 2, 5, 0, 8, 7, 0};
+	int[] arr = new int[]{1, 3, 10, 11, 2, 5, 0, 8, 7};
 	System.out.println(part(arr,0,9));
 	for(int i = 0; i < arr.length; i++){
 	    System.out.print(arr[i]+" ");
