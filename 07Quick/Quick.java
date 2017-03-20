efcbe8a95af7b4c3a979c9ef819763735617e87f
Karen Chen <kchen6600@gmail.com>
2017-03-19 20:41:50 -0400

    deleting commented out code

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -5,16 +5,17 @@ public class Quick{
 	data[first] = data[second];
 	data[second] = t;
     }
-    
-    public static int part(int[] data, int start, int end){
+
+
+     public static int part(int[] data, int start, int end){
 	int pivot = (int)(Math.random() * data.length);
 
-	swap(data, pivot, end-1);
+	swap(data, pivot, end);
 
 	
 	int begin = start;
-	for(int i = start; i < end - 1; i++){
-	    if(data[i] < data[end-1]){ //if element is less than or equal to element at the pivot
+	for(int i = start; i < end; i++){
+	    if(data[i] < data[end]){ //if element is less than or equal to element at the pivot
 		//swap
 		swap(data, begin, i);
 		begin+=1;
@@ -23,20 +24,20 @@ public class Quick{
 	    }
 	}
 	//swap
-	swap(data, begin, end-1);
+	swap(data, begin, end);
 
 	return begin;
 		
     }
 
 
-    public static int quickselect(int[]data, int k){
-	return quickhelper(data, k, 0, data.length);
+    public static int select(int[]data, int k){
+	return quickhelper(data, k, 0, data.length-1);
     }
 
 
     public static int quickhelper(int[]data, int k, int start, int end){
-	int pivotindex = part(data, 0, data.length);
+	int pivotindex = part(data, 0, data.length-1);
 	if (pivotindex == k){
 	    return data[k];
 	}
@@ -47,14 +48,27 @@ public class Quick{
 	    return quickhelper(data, k, pivotindex-1, end);
 	}
     }
+   
     public static void main(String[]args){
 
-	int[] arr = new int[]{1, 3, 10, 11, 2, 5, 0, 8, 7};
-	//System.out.println(part(arr,0,9));
+	//int[] arr = new int[]{1, 0, 10, 11, 2, 2, 5, 0, 8, 7};
+	//System.out.println(part(arr,0,arr.length));
+
+	int[] arr = new int[]{2, 10, 15, 23, 0, 0, 5};
+	System.out.println(select(arr, 0));
+	System.out.println(select(arr, 1));
+	System.out.println(select(arr, 2));
+	System.out.println(select(arr, 3));
+	System.out.println(select(arr, 4));
+	System.out.println(select(arr, 5));
+	/**	
 	for(int i = 0; i < arr.length; i++){
 	    // System.out.print(arr[i]+" ");
 	    System.out.println(quickselect(arr,i));
-	}
+      	}
+	**/
+
+	
 
 	
     }
