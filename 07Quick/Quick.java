5e8cf04c7e120697b68e8c4090abe5958c924e61
Karen Chen <kchen6600@gmail.com>
2017-03-19 22:51:20 -0400

    working

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Quick{
 
     public static void swap(int[]data, int first, int second){
@@ -7,26 +8,35 @@ public class Quick{
     }
 
 
-     public static int part(int[] data, int start, int end){
-	int pivot = (int)(Math.random() * data.length);
+    public static int part(int[] data, int start, int end){
 
-	swap(data, pivot, end);
-
+	if (start == end){
+	    return end;
+	}
 	
-	int begin = start;
-	for(int i = start; i < end; i++){
-	    if(data[i] < data[end]){ //if element is less than or equal to element at the pivot
-		//swap
-		swap(data, begin, i);
-		begin+=1;
+        int pivot = ((int)(Math.random() * (end+1-start))) + start;
+	int pivotnum = data[pivot];
+        swap(data, start, pivot);
 
-	      
+	int begin = start+1;
+	int last = end;
+	while(begin < last){
+	    if(data[begin] < pivotnum){
+		begin+=1;
+	    }
+	    else{
+		swap(data, begin, last);
+		last-=1;
 	    }
 	}
-	//swap
-	swap(data, begin, end);
-
-	return begin;
+	if(data[last] >= data[start]){
+	    swap(data, start, last-1);
+	    return last-1;
+	}
+	else{
+	    swap(data, start, last);
+	    return last;
+	}
 		
     }
 
@@ -37,36 +47,42 @@ public class Quick{
 
 
     public static int quickhelper(int[]data, int k, int start, int end){
-	int pivotindex = part(data, 0, data.length-1);
+	
+	int pivotindex = part(data, start, end);
 	if (pivotindex == k){
 	    return data[k];
 	}
-	else if(k > pivotindex){
-	    return quickhelper(data, k, start, pivotindex-1);
-	}
 	else{
-	    return quickhelper(data, k, pivotindex-1, end);
-	}
+	    if(k > pivotindex){
+		return quickhelper(data, k, pivotindex+1,end);
+	    }
+	    else{
+		return quickhelper(data, k, start, pivotindex-1);
+	    }
+        }
+
     }
    
     public static void main(String[]args){
 
-	//int[] arr = new int[]{1, 0, 10, 11, 2, 2, 5, 0, 8, 7};
-	//System.out.println(part(arr,0,arr.length));
 
-	int[] arr = new int[]{2, 10, 15, 23, 0, 0, 5};
+	//int[] arr = {2, 10, 15, 23, 0, 5};
+
+	int[] arr = {999,999,999,4,1,0,3,2,999,999,999};
+	/**
 	System.out.println(select(arr, 0));
 	System.out.println(select(arr, 1));
 	System.out.println(select(arr, 2));
 	System.out.println(select(arr, 3));
 	System.out.println(select(arr, 4));
 	System.out.println(select(arr, 5));
-	/**	
+	**/
+       
 	for(int i = 0; i < arr.length; i++){
 	    // System.out.print(arr[i]+" ");
-	    System.out.println(quickselect(arr,i));
+	    System.out.println(select(arr,i));
       	}
-	**/
+	
 
 	
 
