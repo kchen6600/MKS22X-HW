b29cc8b9f72f3a5bc7d6c8cbb74d46a9e1dbaebf
Karen Chen <kchen6600@gmail.com>
2017-03-27 19:00:52 -0400

    basic start of linkedlist

---
+++ 09MyLinkedList/MyLinkedList.java
@@ -0,0 +1,14 @@
+public class MyLinkedList{
+
+    LNode start;
+    int size;
+
+    public static void add(){
+    }
+
+    public static String toString(){
+    }
+
+    private class LNode{
+    }
+}
