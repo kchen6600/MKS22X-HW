62ddf9bb25486e6e552896169d3c6976df78f7c8
Karen Chen <kchen6600@gmail.com>
2017-03-28 12:19:50 -0400

    LNode

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,14 +1,35 @@
 public class MyLinkedList{
 
     LNode start;
-    int size;
+    private int size;
+
 
     public static void add(){
+	
+    }
+
+    public static int size(){
+	return size;
     }
 
     public static String toString(){
     }
 
     private class LNode{
+	private int n;
+	private int val;
+
+	public LNode(int value, int next){
+	    val = value;
+	    n = next;
+	}
+
+	public int next(){
+	    return n;
+	}
+
+	public int value(){
+	    return val;
+	}
     }
 }
