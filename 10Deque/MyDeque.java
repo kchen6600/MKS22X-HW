22165e83144fa6d782d6669e13b9abf07a0929aa
Karen Chen <kchen6600@gmail.com>
2017-04-19 12:33:09 -0400

    outline of methods

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,13 +1,25 @@
 public class Deque{
 
-    
+    //add the element to the specified side
+    //double capacity when full
+    public void addFirst (String){
+    }
 
+    public void addLast (String){
+    }
 
+    //retrieve and remove the element from the specified side
+    public String removeFirst(){
+    }
 
+    public String removeLast(){
+    }
 
+    //retrieve element from the specified side
+    public String getFirst(){
+    }
 
-
-
-
+    public String getLast(){
+    }
     
 }
