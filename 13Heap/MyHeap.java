fc1e708acc027af0c3599a1732e094c7d6da80e1
Karen Chen <kchen6600@gmail.com>
2017-05-04 20:34:09 -0400

    outline

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,23 @@
+public class MyHeap{
+
+    public MyHeap(){
+    }
+
+    public MyHeap(boolean boo){
+    }
+
+    public void add(String str){
+    }
+
+    public String remove(){
+    }
+
+    public String peek(){
+    }
+
+    private void pushUp(){
+    }
+
+    private void pushDown(){
+    }
+}
