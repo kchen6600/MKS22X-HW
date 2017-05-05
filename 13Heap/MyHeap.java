ef67d47d82f447f89de9e8d3c0164a79e96fcdb1
Karen Chen <kchen6600@gmail.com>
2017-05-04 20:45:08 -0400

    need to write pushup and pushdown

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,23 +1,62 @@
+import java.util.*;
 public class MyHeap{
 
+    private String[] heaparray;
+    private int size;
+    private int direction; // 1 if max, -1 if min
+
     public MyHeap(){
+	heaparray = new String[10];
+	size = 0;
     }
 
-    public MyHeap(boolean boo){
+    public MyHeap(boolean minmax){
+	if (minmax){
+	    direction = 1;
+	}
+	else{
+	    direction = -1;
+	}
     }
 
     public void add(String str){
+	heaparray[size+1] = str;
+	size += 1;
+	pushUp();
     }
 
     public String remove(){
+	if(size < 1){
+	    throw new IllegalArgumentException();
+	}
+	String removed = heaparray[1];
+	heaparray[1] = heaparray[size];
+	size -= 1;
+	pushDown();
+	return removed;
     }
 
     public String peek(){
+	if (size < 1){
+	    throw new IllegalArgumentException();
+	}
+	return heaparray[1];
     }
 
     private void pushUp(){
     }
 
     private void pushDown(){
+    }
+
+    public String toString(){
+	String str = "[";
+	for (int i = 1; i <= size; i++){
+	    str += heaparray[i];
+	    if (i < size){
+		str += " ";
+	    }
+	}
+	return str += "]";
     }
 }
