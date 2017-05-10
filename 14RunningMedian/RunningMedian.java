05b8685f1ee1730c8f93b0f5f954622adb5a3478
Karen Chen <kchen6600@gmail.com>
2017-05-09 20:06:33 -0400

    skeleton

---
+++ 14RunningMedian/RunningMedian.java
@@ -0,0 +1,27 @@
+public class RunningMedian{
+    //max <= median
+    private MyHeap maxheap;
+    //min >= median
+    private MyHeap minheap;
+    
+    public RunningMedian(){
+	maxheap = new MyHeap();
+	minheap = new MyHeap(false);
+    }
+
+    public double getMedian(){
+    }
+
+    public void add(Integer val){
+    }
+
+    public String toString(){
+    }
+
+    public static void main(String[] args){
+    }
+
+
+
+    
+}
