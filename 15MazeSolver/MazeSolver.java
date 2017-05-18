d340220280a4286bc1656bc9c3b1ab79debadbcd
Karen Chen <kchen6600@gmail.com>
2017-05-18 10:50:39 -0400

    starting skeleton of mazesolver

---
+++ 15MazeSolver/MazeSolver.java
@@ -0,0 +1,21 @@
+public class MazeSolver{
+
+    public MazeSolver(String filename){
+	this(filename, false);
+    }
+
+    public MazeSolver(String filename, boolean animate){
+	//input name of the mazefile
+	//animate
+    }
+
+    public void solve(){
+	solve(1);
+    }
+
+    public void solve(int style){
+    }
+
+    public String toString(){
+    }
+}
