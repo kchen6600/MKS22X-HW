017fc2a5a623f2a0fc41028d5bc2e6cda10bb3e8
Karen Chen <kchen6600@gmail.com>
2017-05-18 23:09:23 -0400

    to be updated

---
+++ 15MazeSolver/Frontier.java
@@ -0,0 +1,14 @@
+public interface Frontier{
+    /**
+    public void add(Location loc){
+	
+    }
+
+    public Location next(){
+	if (this.hasNext()){
+	    return this.next();
+	}
+    }
+    **/
+
+}
